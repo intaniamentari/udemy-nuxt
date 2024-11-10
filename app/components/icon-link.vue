9c44ca94dd20ddf425f8ccc5ac76b261e7e1f3b3
intan <intaniamentari@gmail.com>
2024-11-10 16:53:54 +0800

    beautify UI

--- app/components/icon-link.vue
+++ app/components/icon-link.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="flex space-x-6">
 		<a href="https://github.com/intaniamentari" class="dark:text-white" target="_blank">
-			<Icon class="h-20 dark:text-white" name="grommet-icons:github" style="" />
+			<Icon class="h-20 dark:text-white" name="grommet-icons:github" style="width: 50px;" />
 		</a>
 		<a href="https://www.linkedin.com/in/intania-mentari/" class="dark:text-white" target="_blank">
-			<Icon class="h-20 dark:text-white" name="grommet-icons:linkedin" style="" />
+			<Icon class="h-20 dark:text-white" name="grommet-icons:linkedin" style="width: 43px;" />
 		</a>
 	</div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- You must keep add this for route "/" -->
-	<div class="prose dark:prose-invert prose-img:w-10 prose-a:no-underline prose-h1:text-2xl prose-h2:text-2xl mb-20 max-w-none">
+	<div class="prose dark:prose-invert prose-a:no-underline prose-h1:text-2xl prose-h2:text-2xl mb-20 max-w-none">
 		<ContentDoc />
 		<IconLink />
 	</div>
@@ -12,8 +12,8 @@
 	})
 </script>
 
-<style scoped>
-	h1,h2,h3,h4 {
+<style>
+	h1,h2,h3 {
 		font-family: "Playwrite GB S"; 
 		font-weight: 400
 	} 

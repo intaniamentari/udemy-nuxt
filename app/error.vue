060fab767cddd4a42956f90eb45d1f632527e60d
intan <intaniamentari@gmail.com>
2024-11-10 14:12:44 +0800

    add conde for handle error page

---
+++ app/error.vue
@@ -0,0 +1,19 @@
+<template>
+	<NuxtLayout>
+		<div class="text-center">
+			<div>
+				<h2 class="text-7xl font-bold">{{ error.statusCode }}</h2>
+				<p>{{ error.statusMessage }}</p>
+			</div>
+		</div>
+	</NuxtLayout>
+</template>
+
+<script setup>
+	defineProps({
+		error: Object
+	})
+	useHead({
+		title: error.statusCode
+	})
+</script>

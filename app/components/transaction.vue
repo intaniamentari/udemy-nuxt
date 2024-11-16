4b88bbc53c3f297a928fc791b176aa4e13a9cdbb
intan <intaniamentari@gmail.com>
2024-11-16 13:56:31 +0800

    change arrow by transaction type

--- app/components/transaction.vue
+++ app/components/transaction.vue
@@ -2,7 +2,7 @@
 	<div class="grid grid-cols-2 border-b pb-5 mt-5">
 		<div class="flex items-center justify-between">
 			<div class="flex items-center space-x-1">
-				<UIcon name="i-hugeicons-arrow-up-right-01" class="w-5 h-5 text-green-600" />
+				<UIcon :name="icon" :class="iconColor" class="w-5 h-5 text-green-600" />
 				<p>{{ transaction.description }}</p>
 			</div>
 			<UBadge color="white" variant="solid" v-if="transaction.category">{{ transaction.category }}</UBadge>
@@ -22,7 +22,18 @@
 		transaction: Object
 	})
 
-	console.log(props.transaction)
+	const isIncome = computed(() => {
+		// return props.transaction.type == "Income" || props.transaction.type == "Incvestment" || props.transaction.type == "Saving"
+		return ["Income", "Investment", "Saving"].includes(props.transaction.type);
+	})
+
+	const icon = computed(() => {
+		return isIncome.value ? 'i-hugeicons-arrow-up-right-01' : 'i-hugeicons-arrow-down-left-01'
+	})
+
+	const iconColor = computed(() => {
+		return isIncome.value ? 'dark:text-green-600 text-green-900' : 'dark:text-red-600 text-red-900'
+	})
 
 	const { currency } = useCurrency(props.transaction.amount)
 
